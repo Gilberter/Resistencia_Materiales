0e74b4c0391b04e85337b627e78b07208eab5c6f
DavidZT2222709 <145131653+DavidZT2222709@users.noreply.github.com>
2025-05-28 08:44:11 -0500

    Finalizado

--- app.py
+++ app.py
@@ -16,12 +16,56 @@ def main():
     st.sidebar.header("🔩 Parametros para realzar loz calculos")
 
     st.sidebar.subheader("Eje de transmisión")
-    n_conducido = st.sidebar.number_input("n conducido", value = 0)
-    n_conductor = st.sidebar.number_input("n conductor", value = 0)
+    n_conducido = st.sidebar.number_input("n conducido", min_value = 1)
+    n_conductor = st.sidebar.number_input("n conductor", min_value =  1)
 
     st.sidebar.subheader("🔧 Calculo de torque")
-    pot = st.sidebar.number_input("Potencia(W)", value = 0.0)
-    vel = st.sidebar.number_input("Velocidad de rotación (rpm)", value = 0.0)
+    pot = st.sidebar.number_input("Potencia(W)", min_value = 0.1)
+    vel = st.sidebar.number_input("Velocidad de rotación (rpm)", min_value = 0.1)
     
+    st.sidebar.subheader("🍎 Calculo del momento polar de incercia")
+    diametro = st.sidebar.number_input("Diametro(m)", min_value = 1.0)
+
+    st.sidebar.subheader("Calculo de la tensión cortante")
+    radio = st.sidebar.number_input("Radio(m)", min_value = 1.0)
+
+    st.sidebar.subheader("📐 Calculo de la deformación angular")
+    longitud = st.sidebar.number_input("Longitud(m)", min_value= 1.0)
+    modulo_rigidez = st.sidebar.number_input("Rigidez(MPa)", min_value = 1.0)
+
+    st.sidebar.subheader(" Seleccione el valor de la tensión maxima")
+    tension_max = st.sidebar.number_input("Tensión maxima(MPa)", min_value = 1.0)
+
+    st.sidebar.subheader("Seleccione el material que vamos a usar")
+    material_usar = st.sidebar.selectbox("Material(Limite elastico)", list(materiales.keys()))
+    elastic_limit = materiales[material_usar]
+
+    
+
+
+
+    if st.sidebar.button("🔢 Realizar calculos"):
+        
+        st.subheader("Valores de los calculos")
+
+        eje_transmision_value = eje_transmision(n_conducido, n_conductor)
+        torque = cal_torque(pot, vel)
+        momento_polar = polar_moment_iner(diametro)
+        tension_cor = ten_cortante_fun(torque, radio, momento_polar)
+        deformacion_angular = angular_deformation(torque, longitud, momento_polar, modulo_rigidez)
+        fs = fact_security(elastic_limit, tension_max)
+
+        st.markdown(f"Eje de transmisión = {eje_transmision_value}")
+        st.markdown(f"Torque = {torque:.4f}")
+        st.markdown(f"Momento Polar = {momento_polar:.4f}")
+        st.markdown(f"Tensión Cortante = {tension_cor:.4f}")
+        st.markdown(f"Deformación Angular = {deformacion_angular:.4f}")
+        
+        if fs >= fact_s_min and fs <= fact_s_max:
+            st.info(f"✅ El material seleccionado de acuerdo a los calculos realizados es {materiales} su factor de seguridad es {fs}")
+        else: 
+            st.error(f"⚠️ El material {material_usar} seleccionado NO cumple con el factor de seguridad mínimo ni maximo.")
+
+
 if __name__ == "__main__":
     main()
--- logic.py
+++ logic.py
@@ -1,8 +1,8 @@
-
 import numpy as np
 
 #CONSTANTES
 fact_s_min = 1.5
+fact_s_max = 2.5
 
 #FUNCIONES
 
@@ -26,21 +26,34 @@ def angular_deformation(torque, longitud, polar_moment, rigidez):
 def fact_security(limite_elastico, tension_max):
     return limite_elastico / tension_max
 
-def select_material(tension_max):
-    materiales = {
-        "Acero AISI 1045": 530,
-        "Aluminio 6061-T6": 275,
-        "Bronce SAE 660": 240,
-        "Titanio Grado 5": 895,
-        "Nylon PA66": 90
-    }
+materiales = {
+    "Acero AISI 1045": 530,
+    "Aluminio 6061-T6": 275,
+    "Bronce SAE 660": 240,
+    "Titanio Grado 5": 895,
+    "Nylon PA66": 90,
+    "Acero 8620 cementado":500,
+    "Acero 4140": 655,
+    "Acero 4340": 850,
+    "Acero al carbono 1045": 450,
+    "Fundición nodular": 200,
+    "Bronce al estaño (SAE 600)": 300,
+    "Nylon 6/6 + fibra de vidrio": 180,  
+    "Aleación de aluminio 7075-T6": 500,
+    "Cobre-berilio": 600,
+    "Aleación de aluminio 6061-T6": 240,
+    "PEEK (Polietheretherketone)": 100
+}
 
-    for material, limite in materiales.items():
+"""def select_material(tension_max):
+    materiales_s = materiales;
+
+    for material, limite in materiales_s.items():
         fs = fact_security(limite, tension_max)
         print (f'{material}: FS = {fs:.2f}')
-        if fs >= fact_s_min:
+        if fs >= fact_s_min & fs <= fact_s_max:
             print (f'Material adecuado: {material} (FS = {fs:.2f})')
             return material, fs
         else:
-            print('Ninguno de los materiales dados cumple con el factor de seuridad mínimo.')
-    return None, None           
+            print('Ninguno de los materiales dados cumple con el factor de seguridad mínimo.')
+    return None, None           """
